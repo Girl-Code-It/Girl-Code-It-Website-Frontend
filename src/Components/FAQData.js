d6388cfd4e1247f948552248758e782993b2594c
MiliJolly <66016518+MiliJolly@users.noreply.github.com>
2020-06-27 22:58:47 +0530

    Added Data for Component FAQ

---
+++ src/Components/FAQData.js
@@ -0,0 +1,25 @@
+const Data = [
+  {
+    id: 1,
+    open: false,
+    question: "What is Girl Code It?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidi dunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exerci tation ullamco laboris nisi ut aliquip.  "
+  },
+  {
+    id: 2,
+    open: false,
+    question: "What is Girl Code It?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidi dunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exerci tation ullamco laboris nisi ut aliquip."
+  },
+  {
+    id: 3,
+    open: false,
+    question: "What is Girl Code It?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }
+];
+
+export default Data;
