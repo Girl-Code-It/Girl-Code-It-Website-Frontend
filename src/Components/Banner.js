bbb76f7188e51c602457571e1f689d3618338fcf
prachibansal2000 <56162563+prachibansal2000@users.noreply.github.com>
2020-06-27 22:08:32 +0530

    update banner.js

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,34 @@
 import React from "react";
+import "./Css/Banner.module.css";
 
-export default function Banner() {
-  return <></>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="head">
+          Give wings to
+          <br />
+          your <span className="dreams">dreams</span>
+          <hr />
+          <button className="btn1 border-0">About Us</button>
+          <button className="btn2 border-0">Join Our Community</button>
+        </div>
+        <div className="picture">
+          <img
+            className="code"
+            data-assetindex="0"
+            src="./code.svg"
+            alt="code"
+          />
+
+          <img
+            className="girl"
+            data-assetindex="1"
+            src="./girl.svg"
+            alt="girl"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
