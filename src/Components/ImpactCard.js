815206d29bb247d04bebff0e6c89152c0faabec9
Meghag5 <65459236+Meghag5@users.noreply.github.com>
2020-06-27 22:12:20 +0530

    Update ImpactCard.js

--- src/Components/ImpactCard.js
+++ src/Components/ImpactCard.js
@@ -1,5 +1,27 @@
 import React from "react";
+import "./Css/Impact.module.css";
 
-export default function ImpactCard() {
-  return <></>;
+export default function ImpactCard(props) {
+  return (
+    <div className="col-sm-12 col-lg-6">
+      <div className="one card">
+        <div className="card-block">
+          <div className="left">
+            <img data-assetindex="2" src={props.impact.imgUrl} alt="image1" />
+          </div>
+          <div className="right">
+            <h1>{props.impact.name}</h1>
+            <hr />
+            <p style={{ paddingBottom: "30px" }}>
+              {props.impact.para1} <br />
+              {props.impact.para2}
+            </p>
+            <a href="#" className="btn ">
+              Know More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
