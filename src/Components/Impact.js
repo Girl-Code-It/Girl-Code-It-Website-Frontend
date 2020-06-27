ccafee112603d81bdfc2662db7b257d2c5af667f
Meghag5 <65459236+Meghag5@users.noreply.github.com>
2020-06-27 22:04:31 +0530

    Update Impact.js

--- src/Components/Impact.js
+++ src/Components/Impact.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
 import ImpactCard from "./ImpactCard";
+import "./Css/Impact.module.css";
 
 export default class Impact extends Component {
   render() {
     return (
       <div>
-        <ImpactCard />
-        <ImpactCard />
+        <div className="container padding">
+          <div className="welcome text-center " style={{ width: "100%" }}>
+            <h1>
+              How are we creating an{" "}
+              <span style={{ color: "rgb(0, 141, 200)" }}>Impact</span>?
+            </h1>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <ImpactCard
+              impact={{
+                name: "Opportunity Calendar",
+                imgUrl: "image1.svg",
+                para1: "Know right Opportunities",
+                para2: "at right time!",
+              }}
+            />
+            <ImpactCard
+              impact={{
+                name: "CodeMaps",
+                imgUrl: "image2.svg",
+                para1: "Learn everything the",
+                para2: "right way!",
+              }}
+            />
+            <hr />
+          </div>
+        </div>
       </div>
     );
   }
