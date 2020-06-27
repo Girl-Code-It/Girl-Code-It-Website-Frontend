bcebeedf7c22f71953bca50c6579bd848e33d64b
MiliJolly <66016518+MiliJolly@users.noreply.github.com>
2020-06-27 22:55:57 +0530

    Updated FAQCard.js

--- src/Components/FAQCard.js
+++ src/Components/FAQCard.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function FAQCard() {
-  return <></>;
+export default function FAQCard(props) {
+  return (
+    <div
+      className={"faq" + (props.item.open ? " open" : "")}
+      key={props.item.id}
+      onClick={() => props.handleChange(props.item.id)}
+    >
+      <div className="container-fixed">
+        <div className="faq-question">{props.item.question}</div>
+        <div className="faq-answer">{props.item.answer}</div>
+        <div className="h-divider" />
+      </div>
+    </div>
+  );
 }
