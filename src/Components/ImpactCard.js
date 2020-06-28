50af83ab05717e497794a1876522cd53740bfe01
Meghag5 <65459236+Meghag5@users.noreply.github.com>
2020-06-28 23:39:21 +0530

    Update ImpactCard.js

--- src/Components/ImpactCard.js
+++ src/Components/ImpactCard.js
@@ -1,22 +1,22 @@
 import React from "react";
-import "./Css/Impact.module.css";
+import styles from "./Css/Impact.module.css";
 
 export default function ImpactCard(props) {
   return (
-    <div className="col-sm-12 col-lg-6">
-      <div className="one card">
-        <div className="card-block">
-          <div className="left">
+    <div className={`col-sm-12 col-lg-6`}>
+      <div className={`card ${styles.one}`}>
+        <div className={`card-block`}>
+          <div className={`${styles.left}`}>
             <img data-assetindex="2" src={props.impact.imgUrl} alt="image1" />
           </div>
-          <div className="right">
+          <div className={`${styles.right}`}>
             <h1>{props.impact.name}</h1>
             <hr />
             <p style={{ paddingBottom: "30px" }}>
               {props.impact.para1} <br />
               {props.impact.para2}
             </p>
-            <a href="#" className="btn ">
+            <a href="#" className={`btn`}>
               Know More
             </a>
           </div>
