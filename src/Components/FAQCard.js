7442c9ab16195a89995d4ce056dafaeec95c50dd
MiliJolly <66016518+MiliJolly@users.noreply.github.com>
2020-07-04 16:16:55 +0530

    Updated FAQCard.js

--- src/Components/FAQCard.js
+++ src/Components/FAQCard.js
@@ -1,16 +1,35 @@
 import React from "react";
+import styles from "./Css/FAQS.module.css";
 
 export default function FAQCard(props) {
+  const { open } = props.item;
+  console.log(open);
   return (
     <div
-      className={"faq" + (props.item.open ? " open" : "")}
+      className={`faq`}
       key={props.item.id}
       onClick={() => props.handleChange(props.item.id)}
     >
-      <div className="container-fixed">
-        <div className="faq-question">{props.item.question}</div>
-        <div className="faq-answer">{props.item.answer}</div>
-        <div className="h-divider" />
+      <div className={`container`}>
+        <div className={`row`}>
+          <div className={`col-2 `}>
+            <img
+              className={`${open ? styles.imageopen : styles.image}`}
+              src="./arrow.png"
+              alt=""
+              height="25px"
+            />
+          </div>
+
+          <div className={` col-10 ${styles.faqQuestion}`}>
+            {props.item.question}
+          </div>
+        </div>
+
+        <div className={`${open ? styles.faqAnsweropen : styles.faqAnswer}`}>
+          {props.item.answer}
+        </div>
+        <div className={` ${styles.hdivider}`} />
       </div>
     </div>
   );
