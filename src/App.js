c1da74ea07bcd8903dee80f1e043a26f77995b9f
Kritimukhija <kriti.mukhija@gmail.com>
2020-07-08 19:48:53 +0530

    CodeMap component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from "react";
-import Navbar from "./Components/Navbar";
-import Banner from "./Components/Banner";
-import AboutUs from "./Components/AboutUs";
-import Impact from "./Components/Impact";
 import CodeMap from "./Components/CodeMap";
-import Testimonial from "./Components/Testimonial";
-import FAQS from "./Components/FAQS";
-import QuestionsForm from "./Components/QuestionsForm";
-import Footer from "./Components/Footer";
+
+
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <Banner />
-        <AboutUs />
-        <Impact />
+       
         <CodeMap />
-        <Testimonial />
-        <FAQS />
-        <QuestionsForm />
-        <Footer />
+         
       </div>
     );
   }
--- src/Components/QuestionsForm.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class QuestionsForm extends Component {
-  render() {
-    return <div> </div>;
-  }
-}
