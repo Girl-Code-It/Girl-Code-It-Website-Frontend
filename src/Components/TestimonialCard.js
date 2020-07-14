865cce973eb4428651f6a662c6526aa2147beb52
vaishnavig20 <sweetvaishnavigupta@gmail.com>
2020-07-14 12:29:42 +0530

    feature/testimonial added

--- src/Components/TestimonialCard.js
+++ src/Components/TestimonialCard.js
@@ -1,5 +1,34 @@
 import React from "react";
+import "../Css/testimonialCard.module.css";
 
-export default function TestimonialCard() {
-  return <></>;
+const styleImage = {
+  position: "absolute",
+  width: "100px",
+  transfrom: "translateX(-50%)"
+};
+
+const bg_color = {
+  backgroundColor: "#008dc8",
 }
+
+// pa4-ns
+function TestimonialCard(props) {
+  return (
+    <div className=" box mw7 bg-white br3 pa3 mv5 pb0 ba b--black-10">
+      <div className="imgBox">
+        <img
+          {...styleImage}
+          className="nt5 mw4 mh4 dib br-100 center"
+          src={props.test.imgUrl}
+          alt="nn "
+        />
+      </div>
+
+      <p className="mt1 lh-copy measure center f6">{props.test.detail1}</p>
+      <hr className="center mw3 bb bw1 b--black-10 mv2" />
+      <h3 className="ghg name white" style={bg_color}>{props.test.name}</h3>
+    </div>
+  );
+}
+
+export default TestimonialCard;
