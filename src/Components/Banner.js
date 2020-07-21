d53e062b86753d8f5c34a70fe23c8890470c8df5
prachibansal2000 <prachibansal2111@gmail.com>
2020-07-21 17:27:51 +0530

    (feature/banner)-added banner component

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,38 @@
 import React from "react";
+import styles from "../Css/Banner.module.css";
+import { Container, Row, Col } from "react-bootstrap";
+import code from "../assets/code.svg";
+import girl from "../assets/girl.svg";
 
 export default function Banner() {
-  return <></>;
+  return (
+    <Container>
+      <div className={styles.main}>
+        <Row>
+          <Col xs={12}>
+            <div className={styles.head}>
+              Give wings to
+              <br />
+              your <span className={styles.dreams}>dreams</span>
+              <hr className={styles.hr} />
+            </div>
+          </Col>
+          <Col xs={12}>
+            <div>
+              <img className={styles.code} src={code} alt="code" />
+              <img className={styles.girl} src={girl} alt="girl" />
+              <div className={styles.head}>
+                <button href="#" className={`border-0 ${styles.btn1}`}>
+                  About Us
+                </button>
+                <button href="#" className={`border-0 ${styles.btn2}`}>
+                  Join GCI
+                </button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
 }
