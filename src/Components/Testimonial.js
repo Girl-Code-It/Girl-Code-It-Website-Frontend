7b15afd9bafaf08dae9b2590e495c8f49bdd8a99
vaishnavig20 <sweetvaishnavigupta@gmail.com>
2020-08-04 17:16:50 +0530

    feature/testimonial added

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -1,13 +1,74 @@
 import React, { Component } from "react";
 import TestimonialCard from "./TestimonialCard";
+import Styles from "../Css/testimonial.module.css";
+import "tachyons";
+import Slider from "react-slick";
+import Data from "../assets/data.json";
 
-export default class Testimonial extends Component {
+class Testimonial extends Component {
   render() {
+    const settings = {
+      dots: true,
+      className: "centered",
+      lazyLoad: true,
+      centerMode: false,
+      infinite: true,
+      centerPadding: "30px",
+      slidesToShow: 3, 
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnHover: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            centerMode: true,
+            slidesToShow: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ],
+    };
     return (
-      <div>
-        <TestimonialCard />
-        <TestimonialCard />
+      <div className="wrapper">
+        <h1 className={`mt6 ${Styles.heading}`}>What Our <span className={Styles.highlighted}>Students </span>Say?</h1>
+        <br />
+        <Slider {...settings}>
+          {
+            Data.map((user, i) => {
+              return (
+                <div className="container">
+                  <TestimonialCard 
+                    test = {{
+                      key: `${i}`,
+                      id: `${Data[i].id}`,
+                      name: `${Data[i].name}`,
+                      imgUrl: `https://api.adorable.io/avatars/${Data[i].id}`,
+                      summary: `${Data[i].summary}`
+                    }}
+                  />
+                </div>
+              );
+            })
+          }
+        </Slider>
       </div>
     );
   }
 }
+
+export default Testimonial;
--- src/Components/TestimonialCard.js
+++ src/Components/TestimonialCard.js
@@ -1,5 +1,22 @@
 import React from "react";
+import Styles from "../Css/testimonialCard.module.css";
 
-export default function TestimonialCard() {
-  return <></>;
+function TestimonialCard(props) {
+  return (
+    <div className=" center mw7 bg-white br3 pa3 mv5 pb0 ba b--black-10">
+      <div className="imgBox">
+        <img
+          className={`nt5 mw4 mh4 dib br-100 center ${Styles.displayPicture}`}
+          src={props.test.imgUrl}
+          alt="nn "
+        />
+      </div>
+
+      <p className={`mt1 lh-copy measure center f6 ${Styles.summary}`}>{props.test.summary}</p>
+      <hr className="center mw3 bb bw1 b--black-10 mv2" />
+      <h3 className = {`ghg name white ${Styles.username}`}> {props.test.name} </h3>
+    </div>
+  );
 }
+
+export default TestimonialCard;
