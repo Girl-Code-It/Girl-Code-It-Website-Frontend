700d60744ce4c6b3cef13b3768a42f83a8d4c3bc
Vaishnavi Gupta <sweetvaishnavigupta@gmail.com>
2020-08-07 18:39:35 +0530

    feature/testimonial added

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -1,13 +1,122 @@
 import React, { Component } from "react";
 import TestimonialCard from "./TestimonialCard";
+import "../Css/testimonial.module.css";
+import "tachyons";
+import Slider from "react-slick";
 
-export default class Testimonial extends Component {
+class Testimonial extends Component {
   render() {
+    const settings = {
+      dots: true,
+      className: "centered",
+      lazyLoad: true,
+      centerMode: true,
+      infinite: true,
+      centerPadding: "30px",
+      slidesToShow: 3, 
+      speed: 500,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ],
+    };
+
+    const h1_style = {
+      letterSpacing: "0px",
+      fontSize: "2.3rem",
+      fontWeight: "390",
+      fontFamily: "Arial",
+      textAlign: "center",
+    };
+    
     return (
-      <div>
-        <TestimonialCard />
-        <TestimonialCard />
+      <div className="wrapper">
+        <h1 className="mt6"  style={h1_style}>What Our <span style={{color: '#008dc8'}}>Students </span>Say?</h1>
+        <br />
+        <Slider {...settings}>
+          <div className="container">
+            <TestimonialCard
+              test={{
+                name: "Sherley Williams",
+                imgUrl: "https://picsum.photos/200",
+                detail1:
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos"
+              }}
+            />
+          </div>
+          <div className="container">
+            <TestimonialCard
+              test={{
+                name: "Sherley Williams",
+                imgUrl: "https://picsum.photos/200",
+                detail1:
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos"
+              }}
+            />
+          </div>
+          <div className="container">
+            <TestimonialCard
+              test={{
+                name: "Sherley Williams",
+                imgUrl: "https://picsum.photos/200",
+                detail1:
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos"
+              }}
+            />
+          </div>
+
+          <div className="container">
+            <TestimonialCard
+              test={{
+                name: "Sherley Williams",
+                imgUrl: "https://picsum.photos/200",
+                detail1:
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos"
+              }}
+            />
+          </div>
+          <div className="container">
+            <TestimonialCard
+              test={{
+                name: "Sherley Williams",
+                imgUrl: "https://picsum.photos/200",
+                detail1:
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos"
+              }}
+            />
+          </div>
+          <div className="container">
+            <TestimonialCard
+              test={{
+                name: "Sherley Williams",
+                imgUrl: "https://picsum.photos/200",
+                detail1:
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos"
+              }}
+            />
+          </div>
+        </Slider>
       </div>
     );
   }
 }
+
+export default Testimonial;
