00904c5fed63c1231af2d47341d79af7a8a62f7e
Meghag5 <meghagupta05012001@gmail.com>
2020-08-12 15:42:17 +0530

    (feature/WhyCodeMap-Component) - Added WhyCodeMap-Component

--- src/homepage/Components/Impact.js
+++ src/homepage/Components/Impact.js
@@ -27,7 +27,70 @@ export default class Impact extends Component {
             className="justify-content-md-center"
             style={{ marginTop: "50px" }}
           >
+<<<<<<< Updated upstream:src/homepage/Components/Impact.js
             {ImpactData}
+=======
+            <ImpactCard
+              impact={{
+                name: "Opportunity Calendar",
+                imgUrl: "image1.svg",
+                para1: "Know right Opportunities",
+                para2: "at right time!",
+              }}
+            />
+            <ImpactCard
+              impact={{
+                name: "CodeMaps",
+                imgUrl: "image2.svg",
+                para1: "Learn everything the",
+                para2: "right way!",
+              }}
+            />
+          </Row>
+          <Row
+            className="justify-content-md-center"
+            style={{ marginTop: "5px" }}
+          >
+            <ImpactCard
+              impact={{
+                name: "Open-Source Journey",
+                imgUrl: "image3.svg",
+                para1:
+                  "Get your journey in open-source started with our open-source projects!",
+                para2: "",
+              }}
+            />
+            <ImpactCard
+              impact={{
+                name: "Daily Leetcoding ",
+                imgUrl: "image4.svg",
+                para1: "Solve Leetcode challenges ",
+                para2: "every day with our coders!",
+              }}
+            />
+          </Row>
+          <Row
+            className="justify-content-md-center"
+            style={{ marginTop: "5px" }}
+          >
+            <ImpactCard
+              impact={{
+                name: "Get Career guidance",
+                imgUrl: "image5.svg",
+                para1:
+                  "Confused about your career in tech, Get Personal guidance from experts",
+                para2: "",
+              }}
+            />
+            <ImpactCard
+              impact={{
+                name: "Build Linkedin Profile",
+                imgUrl: "image6.svg",
+                para1: "Learn how to make",
+                para2: "an impressive profile!",
+              }}
+            />
+>>>>>>> Stashed changes:src/Components/Impact.js
             <hr className={styles.line1} />
           </Row>
         </Container>
