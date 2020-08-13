15495b46155c49def4ab7baf9adc2f9b7f0f4f89
harsha14gupta <harsha14gupta@gmail.com>
2020-08-13 19:16:01 +0530

    (fix/WrapperComponents)-Added Wrapper Components

---
+++ src/all_components/Homepage.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Banner from "../homepage/Components/Banner";
+import AboutUs from "../homepage/Components/AboutUs";
+import Impact from "../homepage/Components/Impact";
+import Testimonial from "../homepage/Components/Testimonial";
+import FAQS from "../homepage/Components/FAQS";
+import QuestionsForm from "../homepage/Components/QuestionsForm";
+
+export default class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <Banner />
+        <AboutUs />
+        <Impact />
+        <Testimonial />
+        <FAQS />
+        <QuestionsForm />
+      </div>
+    );
+  }
+}
