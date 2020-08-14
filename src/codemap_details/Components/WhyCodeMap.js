2e386d1f647ee8dd4a44f7986b8373e4a4b8314c
Meghag5 <meghagupta05012001@gmail.com>
2020-08-14 12:14:27 +0530

    (feature/WhyCodeMap) - Added WhyCodeMap Component

--- src/codemap_details/Components/WhyCodeMap.js
+++ src/codemap_details/Components/WhyCodeMap.js
@@ -1,6 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import { Jumbotron, Container } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import box from "../assets/box.svg";
 import styles from "../CSS/WhyCodeMap.module.css";
 
-export default function WhyCodeMap() {
-  return <React.Fragment />;
+export default class WhyCodeMap extends Component {
+  render() {
+    return (
+      
+      <Jumbotron className={styles.background}>
+        <Container fluid="sm">
+          <h1 className={styles.Heading}>
+            <span className={styles.headingColor}>Why </span>CodeMap?
+          </h1>
+          <Card className={styles.Codemap}>
+            <Card.Img
+              className={styles.box}
+              variant="top"
+              src={box}
+              alt="box"
+            />
+            <Card.Body className={styles.content}>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </Card.Text>
+              <Card.Text>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <hr className={styles.line1} />
+        </Container>
+      </Jumbotron>
+
+    );
+  }
 }
+
+
