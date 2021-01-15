91a41e93b718b22d483d0614074766ef4c9e85db
Abdus <abdus@abdus.net>
2021-01-15 23:24:14 +0530

    fix: navlinks (#113)

--- src/commons/Components/Navbar.js
+++ src/commons/Components/Navbar.js
@@ -1,36 +1,26 @@
 import React from "react";
 import styles from "../CSS/Navbar.module.css";
 import { Navbar, Nav } from "react-bootstrap";
-import { Link } from "react-scroll";
+//import { Link } from "react-scroll";
 
 export default function navbar() {
   return (
     <Navbar fixed="top" bg="light" expand="lg" className={styles.NavBar}>
       <Navbar.Brand className={styles.Logo}>
-        <a href = "/" className = {styles.Brand} style = {{fontSize: "35px"}}>
-          Girl 
-          <span style = {{ color: "#008dc8" }}> Code </span> 
+        <a href="/" className={styles.Brand} style={{ fontSize: "35px" }}>
+          Girl
+          <span style={{ color: "#008dc8" }}> Code </span>
           It
         </a>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-          <Link active smooth={true} duration={1000} to="home">
-            Home
-          </Link>
-          <Link smooth={true} offset={-90} duration={1000} to="about">
-            About Us
-          </Link>
-          <Link smooth={true} offset={-190} duration={1000} to="impact">
-            What we Do?
-          </Link>
-          <Link smooth={true} offset={-90} duration={1000} to="faqs">
-            FAQs
-          </Link>
-          <Link smooth={true} offset={-90} duration={1000} to="contact">
-            Contact Us
-          </Link>
+          <a href="/#home">Home</a>
+          <a href="/#about">About Us</a>
+          <a href="/#impact">What we Do?</a>
+          <a href="/#faqs">FAQs</a>
+          <a href="/#contact">Contact Us</a>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
