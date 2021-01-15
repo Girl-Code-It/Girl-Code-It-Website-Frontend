afbb9087e9d39ae49529f70e1da721858a054e22
Vaishali <vaishalithakur614@gmail.com>
2021-01-15 21:16:41 +0530

    Impact Cards  Updated (#110)
    * Impact Cards Reduced
    
    * Updated

--- src/homepage/Components/AboutUs.js
+++ src/homepage/Components/AboutUs.js
@@ -24,11 +24,7 @@ export default function AboutUs({ id }) {
               
               Girl Code It aka GCI, is a group of people who firmly 
               believe in this statement and are eager to build a 
-              healthy coding culture among young coders. In a world 
-              teeming with possibilities due to the rapid growth of 
-              technology everywhere, let's not get left behind in the 
-              rush! GCI is here to help you navigate your way through 
-              the amazing world of computers and coding. 
+              healthy coding culture among young coders. 
 
               <br />
               <br />
