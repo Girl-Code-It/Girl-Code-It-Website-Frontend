4305a264af23be1435e46d13ebd97a31145c5c49
Abdus <abdus@abdus.net>
2021-01-17 11:33:42 +0530

    fix: make cards responsive

--- src/codemap_details/Components/CodeMapCard.js
+++ src/codemap_details/Components/CodeMapCard.js
@@ -7,13 +7,42 @@ function finalUri(id) {
   return `/onecodemap?id=${encodeURIComponent(id)}`;
 }
 
+/*
+ * for some reason, text container's width is not scaling down.
+ * until I figure out what the issue is, following function
+ * will handle it
+ */
+function setTextContainerWidth(parentRef, imgRef, textRef) {
+  const parentWidth = parseFloat(getComputedStyle(parentRef.current).width);
+  const imgWidth = parseFloat(getComputedStyle(imgRef.current).width);
+
+  textRef.current.style.maxWidth = parentWidth - imgWidth - 30 + "px";
+}
+
 export default function CodeMapCard(props) {
+  const imgRef = React.useRef();
+  const textRef = React.useRef();
+  const parentRef = React.useRef();
+
+  React.useEffect(() => {
+    // exec when component gets mounted
+    setTextContainerWidth(parentRef, imgRef, textRef);
+
+    window.addEventListener("resize", () =>
+      setTextContainerWidth(parentRef, imgRef, textRef)
+    );
+  }, []);
+
   return (
-    <Card className={`${styles.card} ${props.className}`}>
+    <Card className={styles.card}>
       <div className={styles.topbar}></div>
-      <Row className="align-items-center">
+      <Row
+        ref={parentRef}
+        className="align-items-center"
+        style={{ flexWrap: "nowrap" }}
+      >
         <Col xs="auto" style={{ textAlign: "center" }}>
-          <Card.Body style={{ paddingRight: 0 }}>
+          <Card.Body ref={imgRef} style={{ paddingRight: 0 }}>
             <Card.Img
               variant="bottom"
               className={styles.img}
@@ -21,9 +50,11 @@ export default function CodeMapCard(props) {
             />
           </Card.Body>
         </Col>
-        <Col xs="auto">
+        <Col xs="auto" ref={textRef}>
           <Card.Body style={{ paddingLeft: 0 }}>
-            <Card.Title className = {styles.Title}>{props.codeMap.name}</Card.Title>
+            <Card.Title className={styles.Title}>
+              {props.codeMap.name}
+            </Card.Title>
             <a
               href={finalUri(props.codeMap.onecodemapLink) || "#"}
               className={styles.button}
