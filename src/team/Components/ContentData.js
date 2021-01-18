436d3ea72cc3b579371802efa90215b9b05947cd
Manvi Tyagi <manvityagi770@gmail.com>
2021-01-18 11:28:56 +0530

    Updated Content Team

--- src/team/Components/ContentData.js
+++ src/team/Components/ContentData.js
@@ -1,23 +1,31 @@
 var ContentData = [
-    {
-        id: 1,
-        name: "Tania Joseph",
-        imgUrl: "Tania.JPG",
-        designation: "Content Team",
-        linkedin: "https://www.linkedin.com/in/tania-joseph-58444a149/",
-        github: "https://github.com/tannyjoseph",
-        bgColor: "#008cd8"
-    },
-    {
-        id: 2,
-        name: "Tanya Batra",
-        imgUrl: "Tanya.jpg",
-        designation: "Content Team",
-        linkedin: "https://www.linkedin.com/in/tanya-batra-97802a131",
-        github: "https://github.com/tanya-b30",
-        bgColor: "#BD6997"
-    }
-  ];
-  
-  export default ContentData;
-  
+  {
+    id: 1,
+    name: "Tania Joseph",
+    imgUrl: "Tania.JPG",
+    designation: "Content Team",
+    linkedin: "https://www.linkedin.com/in/himanshisingla",
+    github: "https://github.com/tannyjoseph",
+    bgColor: "#008cd8",
+  },
+  {
+    id: 2,
+    name: "Himanshi Singla",
+    imgUrl: "Himanshi.jpg",
+    designation: "Content Team",
+    linkedin: "https://www.linkedin.com/in/tania-joseph-58444a149/",
+    github: "",
+    bgColor: "#008cd8",
+  },
+  {
+    id: 3,
+    name: "Tanya Batra",
+    imgUrl: "Tanya.jpg",
+    designation: "Content Team",
+    linkedin: "https://www.linkedin.com/in/tanya-batra-97802a131",
+    github: "https://github.com/tanya-b30",
+    bgColor: "#BD6997",
+  },
+];
+
+export default ContentData;
