57c05e009e8af3055142cdcf3823b42fc17b5a3d
Manvi Tyagi <32908093+Manvityagi@users.noreply.github.com>
2021-03-20 18:46:48 +0530

    updated slack invite (#148)

--- src/codemap_details/Components/Query.js
+++ src/codemap_details/Components/Query.js
@@ -14,20 +14,23 @@ export default function Query() {
           <Row>
             <Col lg={6}>
               <p className={styles.para}>
-                When you join a codemap, you will also be invited to join a 
-                Slack channel which will have your mentors as well as others 
-                who are on the same journey as you.
+                When you join a codemap, you will also be invited to join a
+                Slack channel which will have your mentors as well as others who
+                are on the same journey as you.
                 <br />
                 <br />
-                All your doubts and queries will be addressed in the group, 
-                and any other input you want to give will always be taken into 
-                consideration. So never hesitate to speak up if you have something 
-                interesting to say!
+                All your doubts and queries will be addressed in the group, and
+                any other input you want to give will always be taken into
+                consideration. So never hesitate to speak up if you have
+                something interesting to say!
                 <br />
                 <br />
                 <hr className={styles.line} />
               </p>
-              <a href = "https://girlcodeitdou-cuj7899.slack.com/join/shared_invite/zt-konwwx85-tPNP0mJcnQM2NZjYSSTRxg#/" className = {styles.Button}>
+              <a
+                href="https://join.slack.com/t/girlcodeitdou-cuj7899/shared_invite/zt-ntpa7kh3-wfOeslRHv8uIc_edEgaGxw"
+                className={styles.Button}
+              >
                 Ask here
               </a>
             </Col>
@@ -42,20 +45,23 @@ export default function Query() {
           </h1>
           <Image src={query} className={styles.img} fluid />
           <p className={styles.para}>
-            When you join a codemap, you will also be invited to join a 
-            Slack channel which will have your mentors as well as others 
-            who are on the same journey as you.
+            When you join a codemap, you will also be invited to join a Slack
+            channel which will have your mentors as well as others who are on
+            the same journey as you.
             <br />
             <br />
-            All your doubts and queries will be addressed in the group, 
-            and any other input you want to give will always be taken into 
-            consideration. So never hesitate to speak up if you have something 
+            All your doubts and queries will be addressed in the group, and any
+            other input you want to give will always be taken into
+            consideration. So never hesitate to speak up if you have something
             interesting to say!
             <br />
             <br />
             <hr className={styles.line} />
           </p>
-          <a href = "https://girlcodeitdou-cuj7899.slack.com/join/shared_invite/zt-konwwx85-tPNP0mJcnQM2NZjYSSTRxg#/" className = {styles.Button}>
+          <a
+            href="https://join.slack.com/t/girlcodeitdou-cuj7899/shared_invite/zt-ntpa7kh3-wfOeslRHv8uIc_edEgaGxw"
+            className={styles.Button}
+          >
             Ask here
           </a>
         </Container>
