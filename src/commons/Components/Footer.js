ad193d3b46e35796efa0e9b3578535aed199c4e4
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-05-21 10:40:44 +0530

    responsive footer (#176)

--- src/commons/Components/Footer.js
+++ src/commons/Components/Footer.js
@@ -1,131 +1,109 @@
-import React from "react";
-import { Container, Jumbotron, Row, Col, Image } from "react-bootstrap";
-import Logo from "../Assets/GCI.png"
-import styles from "../CSS/Footer.module.css";
-import { FaLinkedinIn, FaInstagram, FaTwitter, FaPhoneAlt } from "react-icons/fa";
-import { BiCopyright } from "react-icons/bi"
-import { GrMail } from "react-icons/gr";
+import React from 'react';
+import { Container, Jumbotron, Row, Col, Image } from 'react-bootstrap';
+import Logo from '../Assets/GCI.png';
+import styles from '../CSS/Footer.module.css';
+import {
+  FaLinkedinIn,
+  FaInstagram,
+  FaTwitter,
+  FaPhoneAlt,
+  FaGithub,
+} from 'react-icons/fa';
+// import { BiCopyright } from 'react-icons/bi';
+import { GrMail } from 'react-icons/gr';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <Jumbotron className = {styles.Jumbotron}>
-        <Container className = {styles.Container}>
-            <Row className = {styles.Top1}>
-                <Col>
-                    <Image 
-                        className = {styles.Logo}
-                        src = {Logo}
-                        alt = "Logo"
-                    />
-                </Col>
-                <Col className = {styles.Column}>
-                    <h5 className = {styles.Border}> Links </h5>
-                    <p> 
-                        <a href = "https://drive.google.com/file/d/1ClkojwFYH0MJOsPqYhPkjUO1gR-AU1aM/view?usp=sharing" className = {styles.Link}>
-                            Credits 
-                        </a>
-                    </p>
-                    <p> Privacy Policy </p>
-                </Col>
-                <hr className = {styles.line2}/>
-                <Col className = {styles.Column}>
-                    <h5 className = {styles.Border}> Products </h5>
-                    <p> 
-                        <a href = "/codemaps" className = {styles.Link}>
-                            CodeMaps 
-                        </a>
-                    </p>
-                    <p>  
-                        <a href = "/oppcalendar" className = {styles.Link}>
-                            Opportunity Calendar 
-                        </a>
-                    </p>
-                </Col>
-                <hr className = {styles.line2}/>
-                <Col className = {styles.Column}>
-                    <h5 className = {styles.Border}> Contact Us </h5>
-                    <Row>
-                        <Col md = {10}>
-                            <p> <FaPhoneAlt />  (+91) - 96906-29214 </p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md = {10}>
-                            <p> <GrMail />  girlcodeit@gmail.com </p>
-                        </Col>
-                    </Row>               
-                </Col>
-            </Row>
-            {/* <Row className = {styles.Top2}>
-                <Col>
-                    <Image 
-                        className = {styles.Logo}
-                        src = {Logo}
-                        alt = "Logo"
-                    />
-                </Col>
-            </Row> */}
-            <Row className = {styles.Top2}>
-                <Col>
-                    <h5 style ={{textDecoration: "underline"}}> Links </h5>
-                    <p> 
-                        <a href = "https://drive.google.com/file/d/1ClkojwFYH0MJOsPqYhPkjUO1gR-AU1aM/view?usp=sharing" className = {styles.Link}>
-                            Credits 
-                        </a>
-                    </p>
-                    <p> Privacy Policy </p>
-                </Col>
-                <Col>
-                    <h5 style = {{textAlign: "end", textDecoration: "underline"}}> Products </h5>
-                    <p style = {{textAlign: "end"}}> 
-                        <a href = "/codemaps" className = {styles.Link}>
-                            CodeMaps 
-                        </a>
-                    </p>
-                    <p style = {{textAlign: "end"}}> 
-                        <a href = "/oppcalendar" className = {styles.Link}>
-                            Opportunity Calendar 
-                        </a>
-                    </p>
-                </Col>
-            </Row>
-            <Row className = {styles.Top2}>
-                <Col className = {styles.Top3}>
-                    <h5 style ={{textDecoration: "underline"}}> Contact Us </h5>
-                    <Row>
-                        <Col>
-                            <p> <FaPhoneAlt />  (+91) - 96906-29214 </p>
-                        </Col>
-                        <Col>
-                            <p> <GrMail />  girlcodeit@gmail.com </p>
-                        </Col>
-                    </Row>               
-                </Col>
-            </Row>
-            <hr className = {styles.line1}/>
-            <Row className = {styles.Bottom1}>
-                <Col>
-                    <p> <BiCopyright /> 2020 Girl Code It | All rights reserved.</p>
-                </Col>
-                <Col className = {styles.SocialLinks}>
-                    <a href = "https://www.linkedin.com/company/14568052/admin/" className = {styles.Shift}> <FaLinkedinIn /> </a>
-                    <a href = "https://www.instagram.com/girlcodeit/" className = {styles.Shift}> <FaInstagram /> </a>
-                    <a href = "https://twitter.com/GirlCodeIt1" className = {styles.Shift}> <FaTwitter /> </a>
-                </Col>
-            </Row>
-            <Row className = {styles.Bottom2}>
-                <Col>
-                    <p className = {styles.Para}> <BiCopyright /> 2020 Girl Code It | All rights reserved.</p>
-                </Col>
-            </Row>
-            <Row className = {styles.Bottom2}>
-                <Col className = {styles.SocialLinks}>
-                    <a href = "https://www.linkedin.com/company/14568052/admin/" className = {styles.Shift}> <FaLinkedinIn /> </a>
-                    <a href = "https://www.instagram.com/girlcodeit/" className = {styles.Shift}> <FaInstagram /> </a>
-                    <a href = "https://twitter.com/GirlCodeIt1" className = {styles.Shift}> <FaTwitter /> </a>
-                </Col>
-            </Row>
-        </Container>
+    <Jumbotron className={styles.Jumbotron}>
+      <Container className={styles.ContainerFooter}>
+        <Row className={styles.Top1}>
+          <Col className={styles.Column1} style={{ textAlign: 'center' }}>
+            <h5 style={{ fontSize: '22px' }}> Products </h5>
+            <div className={styles.Rectangle} style={{ width: '120px' }}></div>
+            <a href="https://girlcodeit.com/codemaps">
+              <p style={{ fontSize: '18px', color: 'black', cursor: "pointer" }}>CodeMaps</p>
+            </a>
+            <Link to="/">
+              <p style={{ fontSize: '18px', color: "black", cursor: "pointer" }}> Opportuntiy Calendar </p>
+            </Link>
+          </Col>
+
+          <Col className={styles.ImgColumn}>
+            <a href="https://girlcodeit.com/">
+              <Image className={styles.Logo} src={Logo} alt="Logo" />
+            </a>
+          </Col>
+
+          <Col className={styles.Column2} style={{ textAlign: 'center' }}>
+            <h5 style={{ fontSize: '22px' }}> Links </h5>
+            <div className={styles.Rectangle2} style={{ width: '70px' }}></div>
+            <p style={{ fontSize: '18px', color: 'black' }}> Terms and Condition </p>
+            <p style={{ fontSize: '18px', color: 'black' }}> Privacy Policy </p>
+          </Col>
+        </Row>
+
+        <Row className={styles.SocialLinks}>
+          <a
+            data-testid="linkedin"
+            href="https://www.linkedin.com/company/14568052/admin/"
+            className={styles.Shift}
+          >
+            {' '}
+            <FaLinkedinIn />{' '}
+          </a>
+          <a
+            data-testid="instagram"
+            href="https://www.instagram.com/girlcodeit/"
+            className={styles.Shift}
+          >
+            {' '}
+            <FaInstagram />{' '}
+          </a>
+          <a
+            data-testid="twitter"
+            href="https://twitter.com/GirlCodeIt1"
+            className={styles.Shift}
+          >
+            {' '}
+            <FaTwitter />{' '}
+          </a>
+          <a
+            data-testid="github"
+            href="https://github.com/Girl-Code-It"
+            className={styles.Shift}
+          >
+            {' '}
+            <FaGithub />{' '}
+          </a>
+        </Row>
+
+        <Row className={styles.Top1}>
+          <Col className={styles.ContactCol} style={{ textAlign: 'center' }}>
+            <h5> Contact Us </h5>
+            <div className={styles.Rectangle3}></div>
+          </Col>
+        </Row>
+
+        <Row className={(styles.Top1, styles.ContactRow)}>
+          <p>
+            {' '}
+            <FaPhoneAlt /> (+91) - 83848-13292{' '}
+          </p>
+          <p>
+            {' '}
+            <GrMail /> girlcodeit@gmail.com{' '}
+          </p>
+        </Row>
+
+        <Row className={styles.Bottom1}>
+          <Col>
+            <p>
+              &copy; {new Date().getFullYear()} Girl Code It | All rights reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </Jumbotron>
   );
 }
