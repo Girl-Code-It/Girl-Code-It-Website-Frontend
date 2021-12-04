7070ca032ecf4c250d13c24caeb817cd9e8ff403
Vaishali <vaishalithakur614@gmail.com>
2021-12-04 22:07:09 +0530

    Added more events

--- src/events/Components/EventData.js
+++ src/events/Components/EventData.js
@@ -10,6 +10,24 @@ var EventData = [
     },
     {
         id: 2,
+        imgUrl: "GSSoC.png",
+        titlePart1: "GirlScript ",
+        titlePart2: "Summer of Code'21",
+        date: "March 8, 2021",
+        speaker: "Owner : Manvi Tyagi",
+        link: "https://gssoc.girlscript.tech/projects.html"
+    },
+    {
+        id: 3,
+        imgUrl: "Award.jpg",
+        titlePart1: "Best Female Edu. ",
+        titlePart2: "Community",
+        date: "Arpil 4, 2021",
+        speaker: "Awarded by Script Foundation",
+        link: "https://www.linkedin.com/feed/update/urn:li:activity:6841611577695748096"
+    },
+    {
+        id: 4,
         imgUrl: "Ayush_Event.png",
         titlePart1: "Webinar on ",
         titlePart2: "Importance of DSA",
@@ -18,7 +36,7 @@ var EventData = [
         link: "https://youtu.be/rd1WxJBgY_4"
     },
     {
-        id: 3,
+        id: 5,
         imgUrl: "DSA_Buddy.png",
         titlePart1: "DSA Buddy Program ",
         titlePart2: "2021",
